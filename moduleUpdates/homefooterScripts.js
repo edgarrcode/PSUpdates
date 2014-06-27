706feaaccc43c6850193ddfeb45d1c6f28695fc0
Rodriguez <eerodriguez@utepc-144439.utep.edu>
2014-06-27 14:47:11 -0600

    added dropdown to select and show div. Also added the latest version of the CMS module code

---
+++ moduleUpdates/homefooterScripts.js
@@ -0,0 +1,14 @@
+<script>
+jQuery( "body" ).addClass( "heroLayout homePage" );
+jQuery("#_Portals__default_").attr( "href", "#" );
+jQuery(".morelessToggle1").click(function () {
+	jQuery(".showhideText1").toggleClass("showText");
+});
+jQuery(".morelessToggle2").click(function () {
+	jQuery(".showhideText2").toggleClass("showText");
+});
+jQuery(".morelessToggle3").click(function () {
+	jQuery(".showhideText3").toggleClass("showText");
+});
+jQuery( "head" ).prepend( "<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>" );
+</script>
