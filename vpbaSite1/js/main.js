4e52a18d8519d4c1874b224a9b7a4d906ea2c3ea
Rodriguez <eerodriguez@utepc-144439.utep.edu>
2014-08-19 16:50:01 -0600

    created contracts section and added js files for the navigations

--- vpbaSite1/js/main.js
+++ vpbaSite1/js/main.js
@@ -1,7 +1,8 @@
 // OnLoad
 $(function() {
 	$( ".accordion" ).accordion({
-		collapsible: true
+		collapsible: true,
+	    heightStyle: "content" 
 	});
 	$( ".fadeIn" ).animate( {
 		opacity: 1,
@@ -15,17 +16,6 @@ $(function() {
 		top: 0,
 		opacity: 1
 	},250);
-	$(".PSLiveDateCountdown").TimeCircles({
-		circle_bg_color: "#e5e5e5",
-		fg_width: 0.025,
-		bg_width: 1,
-		time: {
-			Days: { color: "#ff6600" },
-			Hours: { color: "#ff6600" },
-			Minutes: { color: "#ff6600" },
-			Seconds: { color: "#ff6600" }
-		}
-	});
 	$( ".mobileNavToggle" ).click(function() {
 			$(".navigationBar").slideToggle("fast", function () {
 			$(".sideBar").toggleClass("mobileNo");
